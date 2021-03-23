c15a7491ec26c3fe5d7087827e24e1677496ea9a
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-03-23 09:51:53 +0100

    Import

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='network-destruction',
+    version='0.0.1',
+    author='Tancredi Orlando',
+    url='https://github.com/turlando/network-destruction',
+
+    classifiers=[
+        ('License :: OSI Approved :: '
+         'GNU Affero General Public License v3 or later (AGPLv3+)')
+    ],
+
+    python_requires='>=3.6, <4',
+
+    packages=['network_destruction'],
+)
