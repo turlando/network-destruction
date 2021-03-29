9b0597e57cc854f5c6e6932c2dd110123fa35b5f
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-03-29 20:12:41 +0200

    Refactor ranking

--- network_destruction/ranking.py
+++ network_destruction/ranking.py
@@ -1,14 +1,17 @@
-from typing import List
+from typing import List, Callable
+
 from dataclasses import dataclass, field
 from math import sqrt
+
 from networkx import Graph, number_connected_components
 from network_destruction.distance import (laplacian_distance,
                                           normalized_laplacian_distance)
+
 from network_destruction.graph import remove_node_edges, giant_order
 
 
 @dataclass
-class GraphRanking:
+class Ranking:
     graph: Graph = field(repr=False)
     isolated_node: int
     laplacian_distance: float
@@ -17,28 +20,53 @@ class GraphRanking:
     giant_order: int
 
 
-def distance_ranking(graph: Graph) -> List[GraphRanking]:
-    def make_ranking(node):
+def distance_ranking(
+        metric: Callable[[Ranking], float]
+) -> Callable[[Graph], List[Ranking]]:
+
+    def make_ranking(graph: Graph, node: int) -> Ranking:
         mutilated_graph = remove_node_edges(graph, node)
-        return GraphRanking(mutilated_graph,
-                            node,
-                            laplacian_distance(graph, mutilated_graph),
-                            normalized_laplacian_distance(graph, mutilated_graph),
-                            number_connected_components(mutilated_graph),
-                            giant_order(mutilated_graph))
+        return Ranking(
+            mutilated_graph,
+            node,
+            laplacian_distance(graph, mutilated_graph),
+            normalized_laplacian_distance(graph, mutilated_graph),
+            number_connected_components(mutilated_graph),
+            giant_order(mutilated_graph)
+        )
 
-    nodes = list(graph.nodes())
-    rankings = [make_ranking(node) for node in nodes]
+    def ranking(graph: Graph) -> List[Ranking]:
+        nodes = list(graph.nodes())
+        rankings = [make_ranking(graph, node) for node in nodes]
 
-    return sorted(rankings,
-                  key=lambda ranking: ranking.normalized_laplacian_distance,
-                  reverse=True)
+        return sorted(rankings, key=metric, reverse=True)
+
+    return ranking
 
 
-def disruption_ranking(graph: Graph, iterations: int = 20):
-    for _ in range(iterations):
-        distances = distance_ranking(graph)
-        best = distances[0]
+laplacian_distance_ranking = distance_ranking(
+    lambda ranking: ranking.laplacian_distance
+)
 
-        graph = best.graph
-        yield best
+normalized_laplacian_distance_ranking = distance_ranking(
+    lambda ranking: ranking.normalized_laplacian_distance
+)
+
+
+def disruption_ranking(distance_ranking: Callable[[Graph], List[Ranking]]):
+    def ranking(graph: Graph, iterations: int = 20):
+        for _ in range(iterations):
+            distances = distance_ranking(graph)
+            best = distances[0]
+
+            graph = best.graph
+            yield best
+
+    return ranking
+
+
+laplacian_disruption_ranking = disruption_ranking(laplacian_distance_ranking)
+
+normalized_laplacian_disruption_ranking = disruption_ranking(
+    normalized_laplacian_distance_ranking
+)
