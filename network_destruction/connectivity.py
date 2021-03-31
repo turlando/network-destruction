0d2411b05cfc499fde96605606583cda15eada4c
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-03-31 13:25:18 +0200

    Add find_probability

---
+++ network_destruction/connectivity.py
@@ -0,0 +1,29 @@
+from typing import TypeVar, Callable
+from network_destruction.graph import make_graph
+from networkx import number_connected_components
+
+
+T = TypeVar('T')
+V = TypeVar('V')
+
+
+def bisect(
+        function: Callable[[V], T],
+        compare: Callable[[T], bool],
+        bisect_value: Callable[[V], V],
+        value: V
+) -> V:
+    result = function(value)
+
+    if compare(result):
+        return value
+    else:
+        new_value = bisect_value(value)
+        return bisect(function, compare, bisect_value, new_value)
+
+
+def find_probability(components: int):
+    return bisect(lambda probability: make_graph(probability=probability),
+                  lambda graph: number_connected_components(graph) >= components,
+                  lambda value: value - value / 100,
+                  0.25)
