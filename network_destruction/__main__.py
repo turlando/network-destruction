9621315a2d8553c0ce31f2caab38748237f061ba
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-04-02 13:51:55 +0200

    Refactor experiment

--- network_destruction/__main__.py
+++ network_destruction/__main__.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as plt
-
 from typing import List
 
 from network_destruction.graph import make_graph
+from network_destruction.plot import Plot, show_plot
 from network_destruction.ranking import (
     Ranking,
     laplacian_disruption_ranking,
@@ -11,167 +10,102 @@ from network_destruction.ranking import (
 )
 
 
-def show_ranking_metrics_comparison(
+def show_laplacian_distance_comparison(
         laplacian_rankings: List[Ranking],
         normalized_laplacian_rankings: List[Ranking],
         giant_order_rankings: List[Ranking],
         probability: float
 ):
-    xs = range(len(laplacian_rankings))
-
-    ##########################################################################
-
-    laplacian_figure, laplacian_axes = plt.subplots()
-    laplacian_axes.set_title(f"Laplacian distance with p={probability}")
-
-    laplacian_axes.plot(
-        xs,
-        [r.laplacian_distance for r in laplacian_rankings],
-        "o-",
-        label="Disruption driven by laplacian distance"
+    show_plot(
+        f"Laplacian distance with p={probability}",
+        Plot("Disruption driven by laplacian distance",
+             range(len(laplacian_rankings)),
+             [r.laplacian_distance for r in laplacian_rankings],
+             [r.isolated_node for r in laplacian_rankings]),
+        Plot("Disruption driven by normalized laplacian distance",
+             range(len(normalized_laplacian_rankings)),
+             [r.laplacian_distance for r in normalized_laplacian_rankings],
+             [r.isolated_node for r in normalized_laplacian_rankings]),
+        Plot("Disruption driven by giant order distance",
+             range(len(giant_order_rankings)),
+             [r.laplacian_distance for r in giant_order_rankings],
+             [r.isolated_node for r in giant_order_rankings])
     )
 
-    for x, ranking in zip(xs, laplacian_rankings):
-        laplacian_axes.annotate(xy=(x, ranking.laplacian_distance),
-                                text=ranking.isolated_node)
 
-    laplacian_axes.plot(
-        xs,
-        [r.laplacian_distance for r in normalized_laplacian_rankings],
-        "o-",
-        label="Disruption driven by normalized laplacian distance"
+def show_normalized_laplacian_distance_comparison(
+        laplacian_rankings: List[Ranking],
+        normalized_laplacian_rankings: List[Ranking],
+        giant_order_rankings: List[Ranking],
+        probability: float
+):
+    show_plot(
+        f"Normalized laplacian distance with p={probability}",
+        Plot("Disruption driven by laplacian distance",
+             range(len(laplacian_rankings)),
+             [r.normalized_laplacian_distance for r in laplacian_rankings],
+             [r.isolated_node for r in laplacian_rankings]),
+        Plot("Disruption driven by normalized laplacian distance",
+             range(len(normalized_laplacian_rankings)),
+             [r.normalized_laplacian_distance for r in normalized_laplacian_rankings],
+             [r.isolated_node for r in normalized_laplacian_rankings]),
+        Plot("Disruption driven by giant order distance",
+             range(len(giant_order_rankings)),
+             [r.normalized_laplacian_distance for r in giant_order_rankings],
+             [r.isolated_node for r in giant_order_rankings])
     )
 
-    for x, ranking in zip(xs, normalized_laplacian_rankings):
-        laplacian_axes.annotate(xy=(x, ranking.laplacian_distance),
-                                text=ranking.isolated_node)
 
-    laplacian_axes.plot(
-        xs,
-        [r.laplacian_distance for r in giant_order_rankings],
-        "o-",
-        label="Disruption driven by giant order distance"
+def show_giant_order_distance_comparison(
+        laplacian_rankings: List[Ranking],
+        normalized_laplacian_rankings: List[Ranking],
+        giant_order_rankings: List[Ranking],
+        probability: float
+):
+    show_plot(
+        f"Giant order distance with p={probability}",
+        Plot("Disruption driven by laplacian distance",
+             range(len(laplacian_rankings)),
+             [r.giant_order_distance for r in laplacian_rankings],
+             [r.isolated_node for r in laplacian_rankings]),
+        Plot("Disruption driven by normalized laplacian distance",
+             range(len(normalized_laplacian_rankings)),
+             [r.giant_order_distance for r in normalized_laplacian_rankings],
+             [r.isolated_node for r in normalized_laplacian_rankings]),
+        Plot("Disruption driven by giant order distance",
+             range(len(giant_order_rankings)),
+             [r.giant_order_distance for r in giant_order_rankings],
+             [r.isolated_node for r in giant_order_rankings])
     )
 
-    for x, ranking in zip(xs, giant_order_rankings):
-        laplacian_axes.annotate(xy=(x, ranking.laplacian_distance),
-                                text=ranking.isolated_node)
 
-    laplacian_axes.set_xticks(xs)
-    laplacian_axes.legend()
-
-    ##########################################################################
-
-    normalized_laplacian_figure, normalized_laplacian_axes = plt.subplots()
-    normalized_laplacian_axes.set_title("Normalized laplacian distance "
-                                        f"with p={probability}")
-
-    normalized_laplacian_axes.plot(
-        xs,
-        [r.normalized_laplacian_distance for r in laplacian_rankings],
-        "o-",
-        label="Disruption driven by laplacian distance"
-    )
-
-    for x, ranking in zip(xs, laplacian_rankings):
-        normalized_laplacian_axes.annotate(
-            xy=(x, ranking.normalized_laplacian_distance),
-            text=ranking.isolated_node
-        )
-
-    normalized_laplacian_axes.plot(
-        xs,
-        [r.normalized_laplacian_distance for r in normalized_laplacian_rankings],
-        "o-",
-        label="Disruption driven by normalized laplacian distance"
-    )
-
-    for x, ranking in zip(xs, normalized_laplacian_rankings):
-        normalized_laplacian_axes.annotate(
-            xy=(x, ranking.normalized_laplacian_distance),
-            text=ranking.isolated_node
-        )
-
-    normalized_laplacian_axes.plot(
-        xs,
-        [r.normalized_laplacian_distance for r in giant_order_rankings],
-        "o-",
-        label="Disruption driven by giant order distance"
-    )
-
-    for x, ranking in zip(xs, giant_order_rankings):
-        normalized_laplacian_axes.annotate(
-            xy=(x, ranking.normalized_laplacian_distance),
-            text=ranking.isolated_node
-        )
-
-    normalized_laplacian_axes.set_xticks(xs)
-    normalized_laplacian_axes.legend()
-
-    ##########################################################################
-
-    giant_order_figure, giant_order_axes = plt.subplots()
-    giant_order_axes.set_title(f"Giant order distance with p={probability}")
-
-    giant_order_axes.plot(
-        xs,
-        [r.giant_order_distance for r in laplacian_rankings],
-        "o-",
-        label="Disruption driven by laplacian distance"
-    )
-
-    for x, ranking in zip(xs, laplacian_rankings):
-        giant_order_axes.annotate(
-            xy=(x, ranking.giant_order_distance),
-            text=ranking.isolated_node
-        )
-
-    giant_order_axes.plot(
-        xs,
-        [r.giant_order_distance for r in normalized_laplacian_rankings],
-        "o-",
-        label="Disruption driven by normalized laplacian distance"
-    )
-
-    for x, ranking in zip(xs, normalized_laplacian_rankings):
-        giant_order_axes.annotate(
-            xy=(x, ranking.giant_order_distance),
-            text=ranking.isolated_node
-        )
-
-    giant_order_axes.plot(
-        xs,
-        [r.giant_order_distance for r in giant_order_rankings],
-        "o-",
-        label="Disruption driven by giant order distance"
-    )
-
-    for x, ranking in zip(xs, giant_order_rankings):
-        giant_order_axes.annotate(
-            xy=(x, ranking.giant_order_distance),
-            text=ranking.isolated_node
-        )
-
-    giant_order_axes.set_xticks(xs)
-    giant_order_axes.legend()
-
-    ##########################################################################
-
-    laplacian_figure.show()
-    normalized_laplacian_figure.show()
-    giant_order_figure.show()
-
-
-def make_comparison(probability: float):
-    print(f"Generating graph with probability={probability}")
+def make_comparison(probability: float, iterations: float):
+    print(f"Generating graph with probability={probability}...", end=' ')
     g = make_graph(probability=probability)
-
-    print("Computing disruption rankings")
-    d_l = list(laplacian_disruption_ranking(g))
-    d_nl = list(normalized_laplacian_disruption_ranking(g))
-    d_go = list(giant_order_disruption_ranking(g))
-
-    show_ranking_metrics_comparison(d_l, d_nl, d_go, probability)
-
     print("Done")
+
+    print("Computing laplacian disruption rankings...", end=' ')
+    d_l = list(laplacian_disruption_ranking(g, iterations))
+    print("Done")
+
+    print("Computing normalized laplacian disruption rankings...", end=' ')
+    d_nl = list(normalized_laplacian_disruption_ranking(g, iterations))
+    print("Done")
+
+    print("Computing giant order disruption rankings...", end=' ')
+    d_go = list(giant_order_disruption_ranking(g, iterations))
+    print("Done")
+
+    print("Showing results...", end=' ')
+    show_laplacian_distance_comparison(d_l, d_nl, d_go, probability)
+    show_normalized_laplacian_distance_comparison(d_l, d_nl, d_go, probability)
+    show_giant_order_distance_comparison(d_l, d_nl, d_go, probability)
+    print("Done")
+
     print()
+
+
+if __name__ == '__main__':
+    make_comparison(0.25, 70)
+    make_comparison(0.10, 70)
+    make_comparison(0.02, 70)
